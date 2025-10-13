9ab681ee8878f6eaa5c78ffa64f3329451cb1227
YlF <ucapaci@ucl.ac.uk>
2025-10-13 10:24:39 +0100

    finished group.py

--- group.py
+++ group.py
@@ -1,5 +1,38 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+my_group = {
+    "name": "vfdv52-Gumanji530",
+    "members": [
+        {"name": "Jill",
+         "age": 26,
+         "job": "biologist",
+         "connections":{ 
+         "friend": ["Zalika"],
+         "partner": ["John"]}
+         },
+        {"name": "Zalika",
+         "age": 28,
+         "job": "Designer",
+         "connections": {
+         "friend": ["Jill"]}
+         },
+        {"name": "John",
+         "age": 27,
+         "job": "Writer",
+         "connections": {
+         "partner": ["Jill"]}
+         },
+        {"name": "Nash",
+         "age": 34,
+         "job": "Chef",
+         "connections": {
+         "cousin": ["John"],
+         "landlord": ["Zalika"]}
+        }
+    ]
+}
 
-my_group =
+# Access example
+print(f"Jill's age: {my_group['members'][0]['age']}")
+print(f"Jill's friends: {my_group['members'][3]['connections']}")
